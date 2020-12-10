409482492bad9ed55868d04da075d6d6dd3eb5a7
Nicolas JENDROWIAK <nico.jendrowiak@gmail.com>
2020-12-10 23:18:35 +0100

    wip

--- Assets/serial.cs
+++ Assets/serial.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.IO;
 using System.IO.Ports;
@@ -8,9 +9,14 @@ using System.IO.Ports;
 public class serial : MonoBehaviour
 {
     private SerialPort _serialPort;
-    private STMData sTMData;
 
-    public GameObject Cube; //The Cube
+    public GameObject Right_leg; //The Cube
+    public Text Text_CAL_Sys;
+    public Text Text_CAL_Gyr;
+    public Text Text_CAL_Acc;
+    public Text Text_CAL_Mag;
+
+    private StreamWriter writer;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,6 +44,10 @@ public class serial : MonoBehaviour
                 
             }
         }
+
+        // Write to disk
+        writer = new StreamWriter("MyPath.csv", true);
+        writer.WriteLine("pitch;roll;yaw");
     }
 
     // Update is called once per frame
@@ -46,27 +56,42 @@ public class serial : MonoBehaviour
         string data = _serialPort.ReadLine();
         Debug.Log(data);
 
-        sTMData = STMData.CreateFromJSON(data);
+        BNOnodes bnos = BNOnodes.CreateFromJSON(data);
+        STMData sTMData = bnos.nodes[0];
 
-        Quaternion rotation = Quaternion.Euler((float)sTMData.euler_roll / 16, (float)sTMData.euler_yaw / 16, (float)sTMData.euler_pitch / 16);
+        // Update angle
+        Quaternion rotation = Quaternion.Euler((float)sTMData.euler_yaw / 16, (float)sTMData.euler_roll / 16, (float)sTMData.euler_pitch / 16);
 
-        Debug.Log((float)sTMData.euler_yaw / 16);
-        
-        Cube.transform.localRotation = rotation;
+        // Debug.Log((float)sTMData.euler_yaw / 16);
+        writer.WriteLine(String.Join(";", new List<double> { sTMData.euler_pitch, sTMData.euler_roll, sTMData.euler_yaw }));
+
+        Right_leg.transform.localRotation = rotation;
+
+        // Update calibration
+        string binary = Convert.ToString(sTMData.calib_state, 2).PadLeft(8, '0');
+        if(binary[7 - 7] == '1' && binary[7 - 6] == '1')
+            Text_CAL_Sys.text = "Sys calibrated";
+        else
+            Text_CAL_Sys.text = "Sys not calibrated";
+        if (binary[7 - 5] == '1' && binary[7 - 4] == '1')
+            Text_CAL_Gyr.text = "Gyr calibrated";
+        else
+            Text_CAL_Gyr.text = "Gyr not calibrated";
+        if (binary[7 - 3] == '1' && binary[7 - 2] == '1')
+            Text_CAL_Acc.text = "Acc calibrated";
+        else
+            Text_CAL_Acc.text = "Acc not calibrated";
+        if (binary[7 - 1] == '1' && binary[7 - 0] == '1')
+            Text_CAL_Mag.text = "Mag calibrated";
+        else
+            Text_CAL_Mag.text = "Mag not calibrated";
     }
 
-    [System.Serializable]
-    class STMData
+    void OnApplicationQuit()
     {
-        public int id;
-        public int calib_state;
-        public double euler_roll;
-        public double euler_pitch;
-        public double euler_yaw;
-
-        public static STMData CreateFromJSON(string jsonString)
-        {
-            return JsonUtility.FromJson<STMData>(jsonString);
-        }
+        Debug.Log("Application ending after " + Time.time + " seconds");
+        writer.Close();
     }
+
+    
 }
